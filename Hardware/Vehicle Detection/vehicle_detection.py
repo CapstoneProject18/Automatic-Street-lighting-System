62d08df4a3fbb606d5fc26ca25101edb3f15bcb1
Archit Kaila <archit.kaila@gmail.com>
2018-11-12 19:29:43 +0530

    Vehicle detection - python imports and csv file read write

---
+++ Hardware/Vehicle Detection/vehicle_detection.py
@@ -0,0 +1,32 @@
+# Imports
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import cv2
+import numpy as np
+import csv
+import time
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+# Object detection imports
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+# initialize .csv
+with open('traffic_measurement.csv', 'w') as f:
+    writer = csv.writer(f)
+    csv_line = \
+        'Vehicle Type/Size, Vehicle Color, Vehicle Movement Direction, Vehicle Speed (km/h)'
+    writer.writerows([csv_line.split(',')])
+
+if tf.__version__ < '1.4.0':
+    raise ImportError('Please upgrade your tensorflow installation to v1.4.* or later!'
+                      )
