f561ca7ce6eeb781d42ebee84d9708b34db382a3
root <ankit.pahuja@st.niituniversity.in>
2018-11-14 20:43:48 +0530

    Login System [View]

---
+++ WebApp/AutoLight/src/AutoLight/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+
+class ComplaintForm(forms.Form):
+	fullname = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				"class":"form-control",
+				"id":"form_full_name",
+				"placeholder":"Your full name please."
+			}
+		)
+	)
+	nearest_location = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				"class":"form-control",
+				"placeholder":"Where is the light affected ? Nearest location please."
+			}
+		)
+	)
+	complaint = forms.CharField(
+		widget=forms.Textarea(
+			attrs={
+				"class":"form-control",
+				"placeholder":"Fill your complaint here please."
+			}
+		)
+	)
+
+class LoginForm(forms.Form):
+	username = forms.CharField(widget=forms.TextInput(
+			attrs={
+				"class":"form-control",
+				"id":"form_full_name"
+			}
+		)
+	)
+	
+	password = forms.CharField(
+		widget=forms.PasswordInput(
+		attrs={
+				"class":"form-control"
+			}
+		)
+	)
--- WebApp/AutoLight/src/AutoLight/views.py
+++ WebApp/AutoLight/src/AutoLight/views.py
@@ -1,11 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import ComplaintForm, LoginForm
 
 def home_page(request):
-	return render(request, "homepage.html", {})
+	contact_form = ComplaintForm(request.POST or None)
+	context = {
+		"form": contact_form
+	}
+	
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	
+	# if request.method == "POST":
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('nearest_location'))
+	# 	print(request.POST.get('complaint'))
 
-def about_page(request):
-	return render(request, "homepage.html", {})
+	return render(request, "homepage.html", context)
 
-def contact_page(request):
-	return render(request, "homepage.html", {})
+def login_page(request):
+	form = LoginForm(request.POST or None)
+	context = {
+		"form": form
+	}
+	print("User logged in")
+	print(request.user.is_authenticated)
+	if form.is_valid():
+		print(form.cleaned_data)
+		username = form.cleaned_data.get("username")
+		password = form.cleaned_data.get("password")
+		user = authenticate(request, username=username, password=password)
+		print(request.user.is_authenticated)
+		if user is not None:
+			print(request.user.is_authenticated)
+			login(request, user)
+			# Redirect to a success page
+			#context['form'] = LoginForm()
+			return redirect("/admin")
+		else:
+			# Return an 'invalid login' error message.
+			print ("Error")
+	return render(request, "auth/login.html", context)
+
+def register_page(request):
+	form = LoginForm(request.POST or None)
+	context = {
+		"form": form
+	}
+	if form.is_valid():
+		print(form.cleaned_data)
+	return render(request, "auth/register.html", context)
+
+
+# def about_page(request):
+# 	return render(request, "homepage.html", {})
+# 
+# def contact_page(request):
+# 	return render(request, "homepage.html", {})
+
